8b15a01855136f1d6c814d187008f666c3d0c05a
Jamz25 <82174996+Jamz25@users.noreply.github.com>
2021-11-21 16:53:47 +0000

    Add World

---
+++ World/World.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+onready var playerSpawn = $PlayerSpawn
+
+func _ready():
+	# instantiate players
+	var player1 = preload("res://Scenes/Player/Player.tscn").instance()
+	player1.set_name(str(get_tree().get_network_unique_id()))
+	player1.set_network_master(get_tree().get_network_unique_id())
+	player1.global_position = playerSpawn.position
+	player1.scale = Vector2(6, 6)
+	add_child(player1)
+	
+	var player2 = preload("res://Scenes/Player/Player.tscn").instance()
+	player2.set_name(str(Globals.player2id))
+	player2.set_network_master(Globals.player2id)
+	player2.global_position = playerSpawn.position
+	player2.scale = Vector2(6, 6)
+	add_child(player2)
