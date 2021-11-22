3b9b270dccc4acc3073d05378a5f14c3225c46c0
Jamz25 <82174996+Jamz25@users.noreply.github.com>
2021-11-22 12:43:25 +0000

    v0.0.2-alpha

--- TitleScreen/TitleScreen.gd
+++ TitleScreen/TitleScreen.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 onready var main_menu = $MainMenu
 onready var settings_menu = $SettingsMenu
+onready var ip_edit = $SettingsMenu/IPEdit
+
+onready var host_menu = $HostingMenu
+onready var host_menu_ip_show = $HostingMenu/IPLabel
 
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
@@ -11,12 +15,16 @@ func _on_Host_button_down():
 	var net = NetworkedMultiplayerENet.new()
 	net.create_server(56231, 4)
 	get_tree().set_network_peer(net)
+	main_menu.visible = false
+	host_menu.visible = true
+	print(IP.get_local_addresses())
+	host_menu_ip_show.text = "Your Local IP: " + str(IP.get_local_addresses()[len(IP.get_local_addresses() ) - 1])
 
 
 func _on_Join_button_down():
 	print("joining game")
 	var net = NetworkedMultiplayerENet.new()
-	net.create_client("192.168.0.160", 56231)
+	net.create_client(ip_edit.text, 56231)
 	get_tree().set_network_peer(net)
 
 
@@ -30,3 +38,7 @@ func _player_connected(id):
 func _on_SettingButton_button_down():
 	main_menu.visible = false
 	settings_menu.visible = true
+
+func _on_BackButton_button_down():
+	main_menu.visible = true
+	settings_menu.visible = false
